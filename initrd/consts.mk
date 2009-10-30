5cc5073c53323d43ed1f0fe992e83224b62ae2c3
Nicolas Rueff <nrueff@mandriva.com>
2009-10-30 12:54:47 +0000

    Pump gets pateched to be constant in fields format (-s) Uses busybox to provide start-stop-daemon and runlevel Now also provides hdparm

--- initrd/consts.mk
+++ initrd/consts.mk
@@ -22,42 +22,43 @@
 #
 
 # PUMP
-PUMP_VERSION		= 0.8.24
+PUMP_VERSION	= 0.8.24
 PUMP_URI		= http://www.linuxfocus.org/~guido
 PUMP_FOLDER		= pump-$(PUMP_VERSION)
-PUMP_TARBALL		= pump-$(PUMP_VERSION)-patched-2009.tar.gz
+PUMP_TARBALL	= pump-$(PUMP_VERSION)-patched-2009.tar.gz
+PUMP_PATCH		= pump-$(PUMP_VERSION)-patched-2009.diff
 
 # POPT
-POPT_VERSION		= 1.15
+POPT_VERSION	= 1.15
 POPT_URI		= http://rpm5.org/files/popt
 POPT_FOLDER		= popt-$(POPT_VERSION)
-POPT_TARBALL		= popt-$(POPT_VERSION).tar.gz
+POPT_TARBALL	= popt-$(POPT_VERSION).tar.gz
 
 # Busybox
-BUSYBOX_VERSION		= 1.1.3
+BUSYBOX_VERSION	= 1.1.3
 BUSYBOX_URI		= http://www.busybox.net/downloads/legacy
-BUSYBOX_FOLDER		= busybox-$(BUSYBOX_VERSION)
-BUSYBOX_TARBALL		= busybox-$(BUSYBOX_VERSION).tar.gz
-BUSYBOX_CONFIG		= busybox-$(BUSYBOX_VERSION).config
+BUSYBOX_FOLDER	= busybox-$(BUSYBOX_VERSION)
+BUSYBOX_TARBALL	= busybox-$(BUSYBOX_VERSION).tar.gz
+BUSYBOX_CONFIG	= busybox-$(BUSYBOX_VERSION).config
 
 # SysVInit
-SYSV_VERSION		= 2.84
+SYSV_VERSION	= 2.84
 SYSV_URI		= ftp://archive.debian.org/debian-archive/debian/pool/main/s/sysvinit
 SYSV_FOLDER		= sysvinit-$(SYSV_VERSION).orig/src
-SYSV_TARBALL		= sysvinit_$(SYSV_VERSION).orig.tar.gz
+SYSV_TARBALL	= sysvinit_$(SYSV_VERSION).orig.tar.gz
 SYSV_PATCH		= sysvinit_$(SYSV_VERSION)-2woody1.diff.gz
 
 # ATFTP
-ATFTP_VERSION		= 0.7
+ATFTP_VERSION	= 0.7
 ATFTP_URI		= ftp://archive.debian.org/debian-archive/debian/pool/main/a/atftp
-ATFTP_FOLDER		= atftp-$(ATFTP_VERSION)
-ATFTP_TARBALL		= atftp_$(ATFTP_VERSION).orig.tar.gz
+ATFTP_FOLDER	= atftp-$(ATFTP_VERSION)
+ATFTP_TARBALL	= atftp_$(ATFTP_VERSION).orig.tar.gz
 ATFTP_PATCH		= atftp_$(ATFTP_VERSION)-7.diff.gz
 
 # LVM
 #MDV/NR LVM_VERSION		= 2.02.54
 LVM_VERSION		= 2.01.04
-#MDV/NR LVM_URI			= ftp://sources.redhat.com/pub/lvm2
+#MDV/NR LVM_URI	= ftp://sources.redhat.com/pub/lvm2
 LVM_URI			= ftp://sources.redhat.com/pub/lvm2/old
 LVM_FOLDER		= LVM2.$(LVM_VERSION)
 LVM_TARBALL		= LVM2.$(LVM_VERSION).tgz
