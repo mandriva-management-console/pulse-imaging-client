c03c5bec8b6cff3bc94ed02ee69be0325a050532
Nicolas Rueff <nrueff@mandriva.com>
2009-10-30 14:19:14 +0000

    atl1e and atl2 are back Added upstream archives

--- kernel/consts.mk
+++ kernel/consts.mk
@@ -42,6 +42,16 @@ URI_ATL1				= ftp://hogchain.net/pub/linux/attansic/kernel_driver
 FOLDER_ATL1				= atl1-$(VERSION_ATL1)
 TARBALL_ATL1			= atl1-$(VERSION_ATL1)-standalone.tar.gz
 
+VERSION_ATL1E			= 1.0.0.9
+URI_ATL1E				= http://partner.atheros.com/Drivers.aspx
+FOLDER_ATL1E			= 459-AR813X-linux-v$(VERSION_ATL1E)/src
+TARBALL_ATL1E			= 459-AR813X-linux-v$(VERSION_ATL1E).tar.gz
+
+VERSION_ATL2			= 2.0.5
+URI_ATL2				= http://partner.atheros.com/Drivers.aspx
+FOLDER_ATL2				= atl2-$(VERSION_ATL2)
+TARBALL_ATL2			= atl2-$(VERSION_ATL2).tar.bz2
+
 VERSION_570X			= 3.99k
 URI_570X				= http://www.broadcom.com/docs/driver_download/570x
 FOLDER_570X				= tg3-$(VERSION_570X)
@@ -59,14 +69,5 @@ URI_SK98				= http://www.marvell.com/drivers/files
 FOLDER_SK98				= DriverInstall
 ARCHIVE_SK98			= Linux_$(VERSION_SK98).zip
 TARBALL_SK98			= install_v$(VERSION_SK98).tar.bz2
-
-
-FOLDER_ATL1e := $(PWD)/3rd_party/atl1e
-SRC_ATL1e := $(FOLDER_ATL1e)/459-AR813X-linux-v1.0.0.9/src
-TARBALL_ATL1e := 459-AR813X-linux-v1.0.0.9.tar.gz
-
-FOLDER_ATL2 := $(PWD)/3rd_party/atl2
-SRC_ATL2 := $(FOLDER_ATL2)/atl2-2.0.5
-TARBALL_ATL2 := atl2-2.0.5.tar.bz2
 
 
