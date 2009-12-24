8706f85f46f1e1381ada1b2da3f96e2f1189f319
Nicolas Rueff <nrueff@mandriva.com>
2009-12-24 10:38:32 +0000

    Bumped kernel version to 2.6.27.42 Dropped 2.6.23-specific patvches (e1000e, igb) Dropped ethernet driver which are now present into the 2.6.27.42 kernel (atl1, atl1e) MSI interrupts activated by default Dropped ISA bus support Dropped PCMCIA bus support Dropped legacy *generic* IDE disk support Activated libata, excepted for HPT 366, Promise 202XX, ALi 5x3 and CMD 64X chipsets which are still using the old stack (libata version too experimental for those)

--- kernel/consts.mk
+++ kernel/consts.mk
@@ -1,60 +1,50 @@
 ARCHITECTURE		= i386
 CC			= gcc-3.4
 
-VERSION_LINUXKERNEL		= 2.6.23.9
+VERSION_LINUXKERNEL	= 2.6.27.42
 URI_LINUXKERNEL     	= http://www.kernel.org/pub/linux/kernel/v2.6
 FOLDER_LINUXKERNEL  	= linux-$(VERSION_LINUXKERNEL)
-TARBALL_LINUXKERNEL 	= linux-$(VERSION_LINUXKERNEL).tar.bz2
-CONFIGS_LINUXKERNEL		= dot-configs
+TARBALL_LINUXKERNEL	= linux-$(VERSION_LINUXKERNEL).tar.bz2
+CONFIGS_LINUXKERNEL	= dot-configs
 
 VERSION_E1000			= 8.0.16
-URI_E1000				= http://downloadmirror.intel.com/9180/eng
+URI_E1000			= http://downloadmirror.intel.com/9180/eng
 FOLDER_E1000			= e1000-$(VERSION_E1000)/src
 TARBALL_E1000			= e1000-$(VERSION_E1000).tar.gz
 
 VERSION_E1000e			= 0.5.18.3
-URI_E1000e				= http://downloadmirror.intel.com/17840/eng
+URI_E1000e			= http://downloadmirror.intel.com/17840/eng
 FOLDER_E1000e			= e1000e-$(VERSION_E1000e)/src
 TARBALL_E1000e			= e1000e-$(VERSION_E1000e).tar.gz
 
-VERSION_IGB				= 2.0.6
-URI_IGB					= http://downloadmirror.intel.com/13663/eng
-FOLDER_IGB				= igb-$(VERSION_IGB)/src
-TARBALL_IGB				= igb-$(VERSION_IGB).tar.gz
+VERSION_IGB			= 2.0.6
+URI_IGB				= http://downloadmirror.intel.com/13663/eng
+FOLDER_IGB			= igb-$(VERSION_IGB)/src
+TARBALL_IGB			= igb-$(VERSION_IGB).tar.gz
 
 VERSION_R8168			= 8.014.00
-URI_R8168				= ftp://WebUser:2mG8dBW@210.51.181.211/cn/nic/
+URI_R8168			= ftp://WebUser:2mG8dBW@210.51.181.211/cn/nic/
 FOLDER_R8168			= r8168-$(VERSION_R8168)/src
 TARBALL_R8168			= r8168-$(VERSION_R8168).tar.bz2
 
 VERSION_R8169			= 6.011.00
-URI_R8169				= ftp://WebUser:2mG8dBW@210.51.181.211/cn/nic
+URI_R8169			= ftp://WebUser:2mG8dBW@210.51.181.211/cn/nic
 FOLDER_R8169			= r8169-$(VERSION_R8169)/src
 TARBALL_R8169			= r8169-$(VERSION_R8169).tar.bz2
 
 VERSION_R8101			= 1.013.00
-URI_R8101				= ftp://WebUser:pGL7E6v@152.104.238.19/cn/nic
+URI_R8101			= ftp://WebUser:pGL7E6v@152.104.238.19/cn/nic
 FOLDER_R8101			= r8101-$(VERSION_R8101)/src
 TARBALL_R8101			= r8101-$(VERSION_R8101).tar.bz2
 
-VERSION_ATL1			= 2.1.3-linux-2.6.23
-URI_ATL1				= ftp://hogchain.net/pub/linux/attansic/kernel_driver
-FOLDER_ATL1				= atl1-$(VERSION_ATL1)
-TARBALL_ATL1			= atl1-$(VERSION_ATL1)-standalone.tar.gz
-
-VERSION_ATL1E			= 1.0.0.9
-URI_ATL1E				= http://partner.atheros.com/Drivers.aspx
-FOLDER_ATL1E			= 459-AR813X-linux-v$(VERSION_ATL1E)/src
-TARBALL_ATL1E			= 459-AR813X-linux-v$(VERSION_ATL1E).tar.gz
-
 VERSION_ATL2			= 2.0.5
-URI_ATL2				= http://partner.atheros.com/Drivers.aspx
-FOLDER_ATL2				= atl2-$(VERSION_ATL2)
+URI_ATL2			= http://partner.atheros.com/Drivers.aspx
+FOLDER_ATL2			= atl2-$(VERSION_ATL2)
 TARBALL_ATL2			= atl2-$(VERSION_ATL2).tar.bz2
 
 VERSION_570X			= 3.99k
-URI_570X				= http://www.broadcom.com/docs/driver_download/570x
-FOLDER_570X				= tg3-$(VERSION_570X)
+URI_570X			= http://www.broadcom.com/docs/driver_download/570x
+FOLDER_570X			= tg3-$(VERSION_570X)
 ARCHIVE_570X			= linux-$(VERSION_570X).zip
 TARBALL_570X			= tg3-$(VERSION_570X).tar.gz
 
