5f7deb4380f173bb9181dfd43b1c6c3f157196fb
Nicolas Rueff <nrueff@mandriva.com>
2010-01-28 15:19:19 +0000

    Code eyecandy

--- bootloader/adjust_length.c
+++ bootloader/adjust_length.c
@@ -1,24 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int  main(int argc,char *argv[]) {
- unsigned char command[132];
- unsigned long lg;
- FILE *fi;
+int main(int argc, char *argv[]) {
+    unsigned char command[132];
+    unsigned long lg;
+    FILE *fi;
 
- system("ls -l revoboot.pxe | awk '{print $5}' >pxe.size");
+    system("ls -l revoboot.pxe | awk '{print $5}' >pxe.size");
 
- fi=fopen("pxe.size","rt");
- fscanf(fi,"%ld",&lg);
- fclose(fi);
+    fi = fopen("pxe.size", "rt");
+    fscanf(fi, "%ld", &lg);
+    fclose(fi);
 
- printf("File length : %ld\n",lg);
- lg-=512;
+    printf("File length : %ld\n", lg);
+    lg -= 512;
 
- fi=fopen("revoboot.pxe","r+b");
- fseek(fi,32,SEEK_SET);
- fwrite(&lg,4,1,fi);
- fwrite(&lg,4,1,fi);
- fclose(fi);
+    fi = fopen("revoboot.pxe", "r+b");
+    fseek(fi, 32, SEEK_SET);
+    fwrite(&lg, 4, 1, fi);
+    fwrite(&lg, 4, 1, fi);
+    fclose(fi);
 
 }
