57fa39592a52cd348e7029f41a228ab9ee3a4567
Nicolas Rueff <nrueff@mandriva.com>
2010-04-14 09:41:05 +0000

    Bumped kernel version to 2.6.32.11 Broadcom tg3 bumped to version 3.105h Broadcom nx2 bumped to version 2-5.0.17 Intel e1000 bumped to version 8.0.19 Intel e1000e bumped to version 1.1.2.1a Intel igb bumped to version 2.1.9 Atheros atl2 removed Atheros atl1e bumped to version 1.0.1.9 Realtek 8101 bumped to version 1.015.00 Realtek 8168 bumped to version 8.018.00 Realtek 8169 bumped to version 6.012.00 Marvel SK98 removed

--- kernel/consts.mk
+++ kernel/consts.mk
@@ -1,68 +1,55 @@
 ARCHITECTURE			= i386
 CC				= gcc-3.4
 
-VERSION_LINUXKERNEL		= 2.6.27.45
+VERSION_LINUXKERNEL		= 2.6.32.11
 URI_LINUXKERNEL     		= http://www.kernel.org/pub/linux/kernel/v2.6
 FOLDER_LINUXKERNEL  		= linux-$(VERSION_LINUXKERNEL)
 TARBALL_LINUXKERNEL		= linux-$(VERSION_LINUXKERNEL).tar.bz2
 CONFIGS_LINUXKERNEL		= dot-configs
 
-VERSION_E1000			= 8.0.16
+VERSION_E1000			= 8.0.19
 URI_E1000			= http://downloadmirror.intel.com/9180/eng
 FOLDER_E1000			= e1000-$(VERSION_E1000)/src
 TARBALL_E1000			= e1000-$(VERSION_E1000).tar.gz
 
-VERSION_E1000e			= 0.5.18.3
-URI_E1000e			= http://downloadmirror.intel.com/17840/eng
+VERSION_E1000e			= 1.1.2.1a
+URI_E1000e			= http://downloadmirror.intel.com/15817/eng
 FOLDER_E1000e			= e1000e-$(VERSION_E1000e)/src
 TARBALL_E1000e			= e1000e-$(VERSION_E1000e).tar.gz
 
-VERSION_IGB			= 2.0.6
+VERSION_IGB			= 2.1.9
 URI_IGB				= http://downloadmirror.intel.com/13663/eng
 FOLDER_IGB			= igb-$(VERSION_IGB)/src
 TARBALL_IGB			= igb-$(VERSION_IGB).tar.gz
 
-VERSION_R8168			= 8.014.00
+VERSION_R8168			= 8.018.00
 URI_R8168			= ftp://WebUser:2mG8dBW@210.51.181.211/cn/nic/
 FOLDER_R8168			= r8168-$(VERSION_R8168)/src
 TARBALL_R8168			= r8168-$(VERSION_R8168).tar.bz2
 
-VERSION_R8169			= 6.011.00
+VERSION_R8169			= 6.012.00
 URI_R8169			= ftp://WebUser:2mG8dBW@210.51.181.211/cn/nic
 FOLDER_R8169			= r8169-$(VERSION_R8169)/src
 TARBALL_R8169			= r8169-$(VERSION_R8169).tar.bz2
 
-VERSION_R8101			= 1.013.00
-URI_R8101			= ftp://WebUser:pGL7E6v@152.104.238.19/cn/nic
+VERSION_R8101			= 1.015.00
+URI_R8101			= ftp://WebUser:2mG8dBW@202.134.71.21/cn/nic
 FOLDER_R8101			= r8101-$(VERSION_R8101)/src
 TARBALL_R8101			= r8101-$(VERSION_R8101).tar.bz2
 
-VERSION_ATL2			= 2.0.5
-URI_ATL2			= http://partner.atheros.com/Drivers.aspx
-FOLDER_ATL2			= atl2-$(VERSION_ATL2)
-TARBALL_ATL2			= atl2-$(VERSION_ATL2).tar.bz2
-
-VERSION_ATL1E			= 1.0.1.6
+VERSION_ATL1E			= 1.0.1.9
 URI_ATL1E			= http://partner.atheros.com/Drivers.aspx
-FOLDER_ATL1E			= AR81Family-linux-v$(VERSION_ATL1E)/src
-TARBALL_ATL1E			= AR81Family-linux-v$(VERSION_ATL1E).tar.gz
+FOLDER_ATL1E			= AR81Family-Linux-v$(VERSION_ATL1E)/src
+TARBALL_ATL1E			= AR81Family-Linux-v$(VERSION_ATL1E).tar.gz
 
-VERSION_570X			= 3.99k
+VERSION_570X			= 3.105h
 URI_570X			= http://www.broadcom.com/docs/driver_download/570x
 FOLDER_570X			= tg3-$(VERSION_570X)
 ARCHIVE_570X			= linux-$(VERSION_570X).zip
 TARBALL_570X			= tg3-$(VERSION_570X).tar.gz
 
-VERSION_NXII			= 1.9.20b_1.50.13
-URI_NXII				= http://www.broadcom.com/docs/driver_download/NXII
-FOLDER_NXII				= netxtreme2-5.0.17/bnx2/src
+VERSION_NXII			= 2.0.8b_1.52.12
+URI_NXII			= http://www.broadcom.com/docs/driver_download/NXII
+FOLDER_NXII			= netxtreme2-5.2.50/bnx2/src
 ARCHIVE_NXII			= linux-$(VERSION_NXII).zip
-TARBALL_NXII			= netxtreme2-5.0.17.tar.gz
-
-VERSION_SK98			= 10.81.3.3
-URI_SK98				= http://www.marvell.com/drivers/files
-FOLDER_SK98				= DriverInstall
-ARCHIVE_SK98			= Linux_$(VERSION_SK98).zip
-TARBALL_SK98			= install_v$(VERSION_SK98).tar.bz2
-
-
+TARBALL_NXII			= netxtreme2-5.2.50.tar.gz
