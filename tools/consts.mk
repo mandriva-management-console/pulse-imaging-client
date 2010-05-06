fb8b025656a3b7f106804bf68ec6295decc66ef1
Yann Droneaud <ydroneaud@mandriva.com>
2010-05-06 08:01:42 +0000

    tools: use system newt  * removed newt  * removed slang  * removed popt

--- tools/consts.mk
+++ tools/consts.mk
@@ -1,15 +1,3 @@
-# NEWT stuff
-NEWT_VERSION	= 0.52.10
-NEWT_URI		= https://fedorahosted.org/releases/n/e/newt
-NEWT_FOLDER		= newt-$(NEWT_VERSION)
-NEWT_TARBALL	= newt-$(NEWT_VERSION).tar.gz
-
-# SLANG stuff
-SLANG_VERSION	= 2.2.1
-SLANG_URI		= ftp://space.mit.edu/pub/davis/slang/v2.2/
-SLANG_FOLDER	= slang-$(SLANG_VERSION)
-SLANG_TARBALL	= slang-$(SLANG_VERSION).tar.gz
-
 # ReiserFS stuff
 REISER_VERSION	= 3.x.0i-linbox01
 REISER_URI		= http://ftp.ens-cachan.fr/ftp/mirror/ReiserFS/reiserfsprogs
@@ -21,12 +9,6 @@ E2FS_VERSION	= 1.22
 E2FS_URI		= http://downloads.sourceforge.net/project/e2fsprogs/e2fsprogs/1.22
 E2FS_FOLDER		= e2fsprogs-$(E2FS_VERSION)
 E2FS_TARBALL	= e2fsprogs-$(E2FS_VERSION).tar.gz
-
-# POPT
-POPT_VERSION	= 1.15
-POPT_URI		= http://rpm5.org/files/popt
-POPT_FOLDER		= popt-$(POPT_VERSION)
-POPT_TARBALL	= popt-$(POPT_VERSION).tar.gz
 
 # MEMTEST86+
 MEMTEST_VERSION = 4.00
