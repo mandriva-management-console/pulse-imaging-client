b637993a88fe7a16d5bd3b271cb3b78ed9a10f2f
Nicolas Rueff <nrueff@mandriva.com>
2010-07-26 13:56:18 +0000

    Added NT Offline stuff

---
+++ postinstall/consts.mk
@@ -0,0 +1,6 @@
+# Offline NT password and registry editor
+
+CHNTPW_VERSION = 100627
+CHNTPW_URI     = http://pogostick.net/~pnh/ntpasswd
+CHNTPW_FOLDER  = chntpw-$(CHNTPW_VERSION)
+CHNTPW_TARBALL = chntpw-source-$(CHNTPW_VERSION).zip
