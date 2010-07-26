d9ff50d86f51537c524a71e1e4cf6941faa22542
Nicolas Rueff <nrueff@mandriva.com>
2010-07-26 14:27:20 +0000

    Added ntfs-3g stuff

--- postinstall/consts.mk
+++ postinstall/consts.mk
@@ -9,3 +9,9 @@ FUSE_VERSION	= 2.8.4
 FUSE_URI	= http://downloads.sourceforge.net/project/fuse/fuse-2.X/$(FUSE_VERSION)
 FUSE_FOLDER	= fuse-$(FUSE_VERSION)
 FUSE_TARBALL	= fuse-$(FUSE_VERSION).tar.gz
+
+# NTFS 3g
+NTFS3G_VERSION	= 2010.5.22
+NTFS3G_URI	= http://tuxera.com/opensource/
+NTFS3G_FOLDER	= ntfs-3g-$(NTFS3G_VERSION)
+NTFS3G_TARBALL	= ntfs-3g-$(NTFS3G_VERSION).tgz
