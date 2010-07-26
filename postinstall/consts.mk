19e7c1232c582cbd59d496dea4eb312f6b833598
Nicolas Rueff <nrueff@mandriva.com>
2010-07-26 14:10:29 +0000

    Added fuse stuff

--- postinstall/consts.mk
+++ postinstall/consts.mk
@@ -1,6 +1,11 @@
 # Offline NT password and registry editor
+CHNTPW_VERSION	= 100627
+CHNTPW_URI	= http://pogostick.net/~pnh/ntpasswd
+CHNTPW_FOLDER	= chntpw-$(CHNTPW_VERSION)
+CHNTPW_TARBALL	= chntpw-source-$(CHNTPW_VERSION).zip
 
-CHNTPW_VERSION = 100627
-CHNTPW_URI     = http://pogostick.net/~pnh/ntpasswd
-CHNTPW_FOLDER  = chntpw-$(CHNTPW_VERSION)
-CHNTPW_TARBALL = chntpw-source-$(CHNTPW_VERSION).zip
+# FUSE
+FUSE_VERSION	= 2.8.4
+FUSE_URI	= http://downloads.sourceforge.net/project/fuse/fuse-2.X/$(FUSE_VERSION)
+FUSE_FOLDER	= fuse-$(FUSE_VERSION)
+FUSE_TARBALL	= fuse-$(FUSE_VERSION).tar.gz
