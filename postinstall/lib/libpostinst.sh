cd75888b28f28077f44c72cd8a4fd06f83e3538c
Cédric Delfosse <cdelfosse@mandriva.com>
2010-10-20 13:21:49 +0000

    Update copyright & license

--- postinstall/lib/libpostinst.sh
+++ postinstall/lib/libpostinst.sh
@@ -1,8 +1,26 @@
-# Global variables which can be intersting in the postinst scripts
+# -*- coding: utf-8; -*-
 #
-# Linbox Rescue Server
-# Copyright (C) 2005-2007 Ludovic Drolez, Linbox FAS
+# (c) 2005-2007 Ludovic Drolez, Linbox FAS
+# (c) 2010 Mandriva, http://www.mandriva.com
 #
+# $Id$
+#
+# This file is part of Pulse 2.
+#
+# Pulse 2 is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Pulse 2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pulse 2.  If not, see <http://www.gnu.org/licenses/>.
+
+# Global variables which can be interesting for the postinst scripts
 
 . /usr/lib/revolib.sh
 . /etc/netinfo.sh
