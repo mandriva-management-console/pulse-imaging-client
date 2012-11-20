b0b8f37c176d1a76ea38079d2162bc8435e6af85
Adam Cécile <acecile@mandriva.com>
2012-11-20 14:16:59 +0100

    Pulse: imaging-client: bump linux kernel version to 3.4.19

--- version.mk
+++ version.mk
@@ -21,7 +21,7 @@
 #
 project = pulse2-imaging-client
 baseversion=1.3.1.1
-linux_version = 3.4.11
+linux_version = 3.4.19
 revision := $(shell if [ -d $(PWD)/.svn ]; then \
 		      rev=`echo $Rev$ | tr -cd [[:digit:]]`; \
 		    elif [ -d $(topdir)/.git ]; then \
