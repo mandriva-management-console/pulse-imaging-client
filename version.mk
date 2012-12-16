1c967dbdd59113427447050b50e85d8ba3318929
Adam Cécile <acecile@mandriva.com>
2012-12-16 21:51:38 +0100

    Pulse imaging client: Bump kernel version to 3.5.10

--- version.mk
+++ version.mk
@@ -20,8 +20,8 @@
 # MA 02110-1301, USA.
 #
 project = pulse2-imaging-client
-baseversion=1.3.1.1
-linux_version = 3.4.19
+baseversion=1.4.0
+linux_version = 3.6.10
 revision := $(shell if [ -d $(PWD)/.svn ]; then \
 		      rev=`echo $Rev$ | tr -cd [[:digit:]]`; \
 		    elif [ -d $(topdir)/.git ]; then \
